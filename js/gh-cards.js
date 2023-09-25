b9dc908c470961f77620a2006d04e7075c8928bb
Kleanthis Avramidis <k.avramidis@windowslive.com>
2023-09-24 23:20:33 -0700

    Update gh-cards.js

--- js/gh-cards.js
+++ js/gh-cards.js
@@ -77,7 +77,6 @@ for(let card of cards) {
   }).then(json => {
     
     card.innerHTML = `
-      <img class="gh" src="${card.getAttribute('data-image') || json.owner.avatar_url}">
       <div class="gh container">
         <h4 class="gh">
           <a class="gh" href="${json.html_url}">
